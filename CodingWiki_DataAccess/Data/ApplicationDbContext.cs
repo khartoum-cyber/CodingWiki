2122ff11deeb7580ddebbe7119996fcc39b82887
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-26 11:54:04 +0200

    first dbSet

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodingWiki_DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
-
+        public DbSet<Book> Books { get; set; }
     }
 }
