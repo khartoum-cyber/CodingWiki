6c8489ee2a461a27d1edddf5ad8aa11287646e5a
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-27 12:02:56 +0200

    connectionString configuration

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -6,5 +6,10 @@ namespace CodingWiki_DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UQA779G;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
     }
 }
