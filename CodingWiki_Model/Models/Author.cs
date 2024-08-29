44e5624be593107ef97f701098ad7f23dd4e27b4
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-29 10:59:38 +0200

    author publisher and subCategory tables

---
+++ CodingWiki_Model/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingWiki_Model.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Author_Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Location { get; set; }
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
