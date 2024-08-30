39dc9135abfa68fd33ec30a38fe2e47dbe8d6ca8
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-08-30 11:43:02 +0200

    Added BookDetails table to db

---
+++ CodingWiki_Model/Models/BookDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingWiki_Model.Models
+{
+    public class BookDetail
+    {
+        [Key]
+        public int BookDetailId { get; set; }
+        [Required]
+        public int NumberOfChapters { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Weight { get; set; }
+    }
+}
