c1218c69f2528e43341dae8af98b5b5af99abea8
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-09-17 08:49:04 +0200

    getAllBooks in console

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+GetAllBooks();
+void GetAllBooks()
+{
+    using var context = new ApplicationDbContext();
+    var books = context.Books.ToList();
+    foreach (var book in books)
+    {
+        Console.WriteLine(book.Title + " - " + book.ISBN);
+    }
+}
